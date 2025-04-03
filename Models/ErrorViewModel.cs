8652e03ff2581f12dce70b842121f12d0c624523
victo <victo@VICSLAPTOP>
2025-04-03 17:40:01 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022PM650_2022SG650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/libreriaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022PM650_2022SG650.Models
+{
+    public class libreriaDbContext : DbContext
+    {
+        public libreriaDbContext(DbContextOptions<libreriaDbContext> options) : base(options)
+        { 
+
+        }
+    }
+}
