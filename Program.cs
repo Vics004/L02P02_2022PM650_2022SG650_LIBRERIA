b61d3b6453063004f00db12e86a65bcd391a06db
Alejandra <alejandrasandoval4321@gmail.com>
2025-04-03 21:18:26 -0600

    Fase final del proyecto

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Ventas}/{action=IniciarVenta}/{id?}");
 
 app.Run();
